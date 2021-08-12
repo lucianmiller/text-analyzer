3a83393aa414dc4009846b0c64744d03a32ac2a9
Lucian <lucian.miller185@gmail.com>
2021-08-12 09:32:59 -0700

    It will make code DRY

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,8 @@
+// Utility Logic-------------
+function noInputtedWord(word, text) {
+  return ((text.trim().length === 0) || (word.trim().length === 0));
+}
+
 //Business Logic-------------
 function wordCounter(text) {
   if (text.trim().length === 0) {
@@ -14,7 +19,7 @@ function wordCounter(text) {
 }
 
 function numberOfOccurrencesInText(word, text) {
-  if ((text.trim().length === 0) || (word.trim().length === 0)) {
+  if (noInputtedWord(word, text)) {
     return 0;
   }
   const wordArray = text.split(" ");
@@ -29,15 +34,20 @@ function numberOfOccurrencesInText(word, text) {
 
 // User Interface Logic--------------
 function boldPassage(word, text) {
-  let htmlString = "<p>"
-  let textArray = text.split(" ")
-  textArray.forEach(function(element) {
-    if (word === element) {
+  if (noInputtedWord(word, text)) {
+    return "";
+  }
+  let htmlString = "<p>";
+  let textArray = text.split(" ");
+  textArray.forEach(function(element, index) {
+    if (element.toLowerCase().includes(word.toLowerCase())) {
       htmlString = htmlString.concat("<b>" + element + "</b>");
     } else {
       htmlString = htmlString.concat(element);
     }
-    htmlString = htmlString.concat(" ");
+    if (index !== (textArray.length - 1)) {
+      htmlString = htmlString.concat(" ");
+    }
   });
   return htmlString + "</p>";
 }
@@ -51,5 +61,6 @@ $(document).ready(function() {
     const occurrenceOfWord = numberOfOccurrencesInText(word, passage);
     $("#total-count").html(wordCount);
     $("#selected-count").html(occurrenceOfWord);
+    $("#bolded-passage").html(boldPassage(word, passage))
   });
 });
